a0f46e17d64dd9fb0a7b9de78f1026488c59fe7f
gemhunter178 <gemhunter178@users.noreply.github.com>
2021-03-03 00:55:43 +0000

    Jekyll build from Action 04348d308eef3f71aaeba965c639873d01e85ade

--- nb/typo_1.js
+++ nb/typo_1.js
@@ -1,45 +1,48 @@
 /* Tell ESLint that there will be the following global variables */
 /* global text:true */
-
-let iptNum = 1;
-const firstWord = text.replace(/ .*/, '');
-if (firstWord.replace(/[0-9]/g, '') === 'num') {
-  const value = firstWord.match(/\d+$/);
-  if (value) {
-    iptNum = parseInt(value);
-    if (isNaN(iptNum)) {
-      iptNum = 1;
-    }
-    text = text.substr(text.indexOf(' ') + 1);
-  }
+var iptNum = 1;
+if(text.length==0){
+	text="You gotta enter a sentence to typo, silly!";
+} else if(/^help$|^\\?$|^info$/.test(text)){
+	text="Gem's typo v1! allows even more cursed typos if you start with typo[num], up to 10";
+	iptNum=0;
+} else {
+	const rgex=/typo(\d+)\s/;
+	let fndwrd = text.match(rgex);
+	let pos = text.search(rgex);
+	if(fndwrd!=null){
+		iptNum=fndwrd[1];
+		text = text.substr(0,pos)+text.substr(pos+fndwrd[0].length);
+	} else if(text.match(/typo(\d+)/)) {
+		text = "You can't just enter a typo level without a sentence!";
+	}
+}
+	
+var repeatNum = 0;
+if(iptNum>10||iptNum<0){
+	repeatNum = 4;
+} else if(iptNum > 6){
+	repeatNum = iptNum - 6;
 }
 
-if (text.length === 0) {
-  text = 'You gotta enter a sentence to typo, silly!';
-} else if (/^help$|^\\?$|^info$/.test(text)) {
-  text = "Gem's typo v1! allows even more cursed typos if you start with num[num]";
-  iptNum = 0;
-}
-
-if (iptNum > 6 || iptNum < 0) {
-  iptNum = 6;
-}
-
-const words = text.split(' ');
-for (let i = 0; i < words.length; i++) {
-  const ltrs = words[i].split('');
-  for (let j = 1; j < ltrs.length - 1; j++) {
-    if (Math.random() < (0.15 * iptNum)) {
-      if (Math.random() < 0.7) {
-        const tmp = ltrs[j];
-        ltrs[j] = ltrs[j + 1];
-        ltrs[j + 1] = tmp;
-        j++;
-      } else {
-        ltrs[j] += ltrs[j];
-      }
-    }
-  }
-  words[i] = ltrs.join('');
+var words = text.split(' ');
+for(i=0;i<words.length;i++){
+	let ltrs = words[i].split('');
+	for(j=1;j<ltrs.length-1;j++){
+		if(Math.random()<(0.15*iptNum)){
+			if(Math.random()<0.7){
+				tmp=ltrs[j];
+				ltrs[j]=ltrs[j+1];
+				ltrs[j+1]=tmp;
+				j++;
+			}else{
+				ltrs[j]+=ltrs[j];
+			}
+		}
+		if(j>1 && Math.random()<(.025*repeatNum)){
+			j--;
+		}
+	}
+	words[i]=ltrs.join('');
 }
 text = words.join(' ');
