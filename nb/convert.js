6d360243c0b2c3b403ecfe41870aefab2c7c7b2d
gemhunter178 <gemhunter178@users.noreply.github.com>
2021-04-25 03:18:41 +0000

    Jekyll build from Action 3e0c93f1495a5b0e543fbb05408cefb44af1ee4a

--- nb/convert.js
+++ nb/convert.js
@@ -216,7 +216,7 @@ if (helpTrg !== 0) {
         quart: 'qt',
         quarts: 'qt',
 		pint: 'pt',
-		pints 'pt',
+		pints: 'pt',
         cup: 'c',
         cups: 'c',
         fluidounce: 'floz',
