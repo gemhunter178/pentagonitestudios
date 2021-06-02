4b727496399691e5f9a6d64d4c60d6899193b869
Gem <gemhunter178@gmail.com>
2021-06-02 15:09:25 -0400

    Create johnmulaney.js

---
+++ _nb/johnmulaney.js
@@ -0,0 +1,42 @@
+---
+description: |
+  [CONTAINS EXPLETIVES] various quotes by John Mulaney... but out of context in one array called "_jm_".
+variables:
+  - jm
+tag: large arrays
+order: 5
+---
+/* eslint-disable no-trailing-spaces */
+
+const jm = ["Ooooooh... I'm sorryyyyyyyyyyyyy",
+  "Nah sister, you're not getting me to no secondary location. You want it? Go GET IT",
+  'eat ass, suck a dick and sell drugs',
+  "Now you've thrown him off his RHYTHM",
+  "I'm like 'NO! That's the thing I'm sensitive about!'",
+  'She is a dynamite, 5-foot Jewish bitch and she is the BEST',
+  'Cause he never forgets a bitch...EVER',
+  'You have the moral backbone of a chocolate eclair',
+  "You're a little fat girl aren't you???",
+  'SCATTER',
+  "I'm very gay, I'd like a few dollars",
+  'Is that Dean Cain?',
+  "Give us some money... as a gift! We want a gift... but only if it's MONEY",
+  '...to begin with a BIG ASS "H"!!',
+  "excuse me I am homeless, I am gay, I have AIDS... I'm new in town",
+  "My wife's in love with this Timothée Chalamet son of a bitch",
+  'do my friends hate me or do I just need to go to sleep?',
+  'STREET SMARTS',
+  "Awwwwww.....she's ugly!",
+  "They've never spoken",
+  "stop snitchin' MOTHAFUCKA",
+  "and I said 'no', y'know...like a liar",
+  'We have to go...we have to go see Bill',
+  'Too old to be a duckling? Quack quack!',
+  'Was there even a ghost, mother... or was the dead Victorian girl you saw just me all along?',
+  'Ooohhhh yes',
+  'NOT FUNNY',
+  'I had a good alibi since I was in Wisconsin and 12',
+  'I am very small and I have no money... So can you imagine the kind of stress that I am under',
+  'No, because I was over on the bench',
+  "That's what I thought you said you dumb fucking horse!",
+  'Get some rest, tall child!!!'];
