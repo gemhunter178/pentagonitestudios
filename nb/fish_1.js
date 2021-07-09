78fdfd12700b488526038d5a053df7e9ced69542
gemhunter178 <gemhunter178@users.noreply.github.com>
2021-07-09 03:39:57 +0000

    Jekyll build from Action ce0929ce92d48d49b23d7f65621299214977e9a8

--- nb/fish_1.js
+++ nb/fish_1.js
@@ -41,6 +41,9 @@ if ((Math.random()) < 0.9) {
     sz = 1;
   }
   fishoutput = user + ' caught a fish of ' + fsh.toFixed(2) + 'kg! (' + (fsh * 2.20462).toFixed(2) + 'lbs) ' + msg[sz];
+  if(/6[.]*9/.test(fishoutput)){
+    fishoutput += "... nice. ";
+  }
 } else {
   fishoutput = 'Oh no! The fish escaped!! lycelHands ';
 }
