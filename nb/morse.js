f87537e66546b401326f40e978760c2e09e1d702
gemhunter178 <gemhunter178@users.noreply.github.com>
2021-07-16 00:02:58 +0000

    Jekyll build from Action 263f4ef2bef4712e7369ef0d126ac0fbcc882a5c

--- nb/morse.js
+++ nb/morse.js
@@ -57,7 +57,7 @@ if (text === '') {
         text[i] = toWord[text[i]];
       }
     }
-    text = ' ' + text.join('');
+    text = '⠀' + text.join('');
   } else {
     // probably a word to morse
     text = text.toUpperCase();
