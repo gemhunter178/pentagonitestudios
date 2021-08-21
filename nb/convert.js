ff0bef14fe4a1e366ed4b36918d448bdf2463271
gemhunter178 <gemhunter178@users.noreply.github.com>
2021-08-21 01:41:36 +0000

    Jekyll build from Action 80634c2fc6dc68c9d7333d31d0b0080643ae9453

--- nb/convert.js
+++ nb/convert.js
@@ -246,6 +246,7 @@ const conversions = (function () {
       gram: 'g',
       grams: 'g',
       tons: 'ton',
+      tonne: 'metric_ton',
       lb: 'lbs',
       pound: 'lbs',
       pounds: 'lbs',
